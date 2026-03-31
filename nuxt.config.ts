7929f02c08e53dbe6ad0c4c873684651fdc2b9b5
JasperLHJ <jasperleehaujun88@gmail.com>
2026-03-31 18:34:57 +0800

    CHORE: Changes to email and linkedin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     resendApiKey: '',
-    contactToEmail: 'hello@jasperlhj.dev',
+    contactToEmail: 'jasperleehaujun88@gmail.com',
   },
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
